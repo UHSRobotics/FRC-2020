04c4e69298bea92852f92808b56901cb4fbd0854
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-09 11:45:48 -0500

    renamed variables and removed unused comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,7 @@ import static edu.wpi.first.wpilibj.XboxController.Button;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final FlywheelSubsystem m_robotDrive = new FlywheelSubsystem();
+  private final FlywheelSubsystem m_flywheelSubsystem = new FlywheelSubsystem();
   private final ColorSubsystem m_colorSubsystem = new ColorSubsystem();
   private final SpinSubsystem m_spinSubsystem = new SpinSubsystem();
 
@@ -36,11 +36,10 @@ public class RobotContainer {
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    // Configure the button bindings
     configureButtonBindings();
-    m_robotDrive.setDefaultCommand(
+    m_flywheelSubsystem.setDefaultCommand(
         new FlywheelDebugCommand(
-            m_robotDrive,
+            m_flywheelSubsystem,
             () -> m_driverController.getY(GenericHID.Hand.kLeft),
             () -> m_driverController.getY(GenericHID.Hand.kRight)));
 
