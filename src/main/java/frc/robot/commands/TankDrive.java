68e48c821aba1bb32b69a1ddc2c9ec790f3e1cf7
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-14 18:06:55 -0500

    fixed tankdrive

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
@@ -21,14 +22,15 @@ public class TankDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_drive.arcadeDrive(Math.abs(m_left.getAsDouble()) > Constants.joystickDeadzone ? m_left.getAsDouble() : 0,
+    // SmartDashboard.putNumber("Controller axis ", m_left.getAsDouble());
+    m_drive.tankDrive(Math.abs(m_left.getAsDouble()) > Constants.joystickDeadzone ? m_left.getAsDouble() : 0,
         Math.abs(m_right.getAsDouble()) > Constants.joystickDeadzone ? m_right.getAsDouble() : 0);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_drive.arcadeDrive(0, 0);
+    m_drive.tankDrive(0, 0);
   }
 
   // Returns true when the command should end.
