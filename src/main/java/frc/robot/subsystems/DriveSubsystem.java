6ae1e69acdfc76c65609f654036f9f69cf91569b
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-15 17:12:48 -0500

    addde ability to change speed multiplier of drivesubsystem in shuffleboard

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -11,6 +11,9 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -20,6 +23,10 @@ public class DriveSubsystem extends SubsystemBase {
   private final VictorSPX m_leftBackMotor = new VictorSPX(2);
   private final VictorSPX m_rightBackMotor = new VictorSPX(0);
 
+  private final ShuffleboardTab tab = Shuffleboard.getTab("Drive");
+  private NetworkTableEntry speedEntry;
+  private double speedMultiplier = 1;
+
   public DriveSubsystem() {
     m_leftFrontMotor.setNeutralMode(NeutralMode.Coast);
     m_rightFrontMotor.setNeutralMode(NeutralMode.Coast);
@@ -28,20 +35,42 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   public void tankDrive(double l, double r) {
-    // l*=0.5;
-    // r*=0.5;
+    l *= speedMultiplier;
+    r *= speedMultiplier;
     m_leftFrontMotor.set(ControlMode.PercentOutput, -l);
     m_leftBackMotor.set(ControlMode.PercentOutput, -l);
     m_rightFrontMotor.set(ControlMode.PercentOutput, r);
-    m_rightBackMotor.set(ControlMode.PercentOutput,r);
+    m_rightBackMotor.set(ControlMode.PercentOutput, r);
   }
 
   public void arcadeDrive(double pow, double turn) {
-    // pow*=0.5;
-    // turn*=0.5;
-    m_leftFrontMotor.set(ControlMode.PercentOutput, -pow+turn);
-    m_leftBackMotor.set(ControlMode.PercentOutput, -pow+turn);
-    m_rightFrontMotor.set(ControlMode.PercentOutput, pow+turn);
-    m_rightBackMotor.set(ControlMode.PercentOutput, pow+turn);
+    pow *= speedMultiplier;
+    turn *= speedMultiplier;
+    m_leftFrontMotor.set(ControlMode.PercentOutput, -pow + turn);
+    m_leftBackMotor.set(ControlMode.PercentOutput, -pow + turn);
+    m_rightFrontMotor.set(ControlMode.PercentOutput, pow + turn);
+    m_rightBackMotor.set(ControlMode.PercentOutput, pow + turn);
+  }
+
+  public void setSpeedMultiplier(double speed, boolean updateNT) {
+    if (0 <= speed && speed <= 2) {
+      speedMultiplier = speed;
+      if(updateNT){
+        System.out.println("Putted Speed Multiplier NT entry");
+        speedEntry.setDouble(speedMultiplier);
+      }
+    } else {
+      System.out.println("Putted Speed Multiplier NT entry");
+      speedEntry.setDouble(speedMultiplier);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    if(speedEntry==null){
+      speedEntry = tab.add("Speed Multiplier", 1).getEntry();
+      System.out.println("Added Speed Multiplier NT entry");
+    }
+    setSpeedMultiplier(speedEntry.getDouble(1.0), false);
   }
 }
