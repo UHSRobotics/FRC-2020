a098f29ba412a996eac85e312becd0826f37262d
CheatCod <2892084432j@gmail.com>
2020-01-21 18:02:12 -0500

    TESTING, will integrade to json class soon

---
+++ src/main/java/frc/robot/Color.java
@@ -0,0 +1,13 @@
+package frc.robot;
+public class Color {
+    public int r;
+    public int g;
+    public int b;
+
+    public Color(int r, int b, int g) {
+        this.r = r;
+        this.b = b;
+        this.g = g;
+    }
+    
+}
