5b7893bfd0a4cd4151c806bf3591963eba809185
Edge7481 <owenhu0410@gmail.com>
2020-02-07 16:37:59 -0500

    using 4 motors

--- src/main/java/frc/robot/subsystems/TalonFXDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/TalonFXDriveSubsystem.java
@@ -21,18 +21,32 @@ import frc.robot.Constants;
 public class TalonFXDriveSubsystem extends SubsystemBase {
     private final TalonFX m_leftMotor = new TalonFX(1);
     private final TalonFX m_rightMotor = new TalonFX(3);
+    private final TalonFX m_leftFollowMotor = new TalonFX(2);
+    private final TalonFX m_rightFollowMotor = new TalonFX(0);
 
     private final ShuffleboardTab tab = Shuffleboard.getTab("Drive (Falcon 500)");
     private NetworkTableEntry speedEntry;
     private double speedMultiplier = 0.5;
 
     public TalonFXDriveSubsystem() {
+        // set motors to coast
         m_leftMotor.setNeutralMode(NeutralMode.Coast);
         m_rightMotor.setNeutralMode(NeutralMode.Coast);
+        m_leftFollowMotor.setNeutralMode(NeutralMode.Coast);
+        m_rightFollowMotor.setNeutralMode(NeutralMode.Coast);
+        // establish master
+        m_leftFollowMotor.follow(m_leftMotor);
+        m_rightFollowMotor.follow(m_rightMotor);
+        // invert motor
         m_leftMotor.setInverted(true);
         m_rightMotor.setInverted(false);
+        m_leftFollowMotor.setInverted(InvertType.FollowMaster);
+        m_rightFollowMotor.setInverted(InvertType.FollowMaster);
+        // reset encoder
         m_leftMotor.getSensorCollection().setIntegratedSensorPosition(0, 0);
         m_rightMotor.getSensorCollection().setIntegratedSensorPosition(0, 0);
+        m_leftFollowMotor.getSensorCollection().setIntegratedSensorPosition(0, 0);
+        m_rightFollowMotor.getSensorCollection().setIntegratedSensorPosition(0, 0);
     }
 
     public void arcadeDrive(double pow, double turn) {
@@ -41,9 +55,6 @@ public class TalonFXDriveSubsystem extends SubsystemBase {
     }
 
     public void encoderTest() {
-        double lPosition = m_leftMotor.getSensorCollection().getIntegratedSensorPosition();
-        double rPosition = m_rightMotor.getSensorCollection().getIntegratedSensorPosition();
-        double lVelocity = m_leftMotor.getSensorCollection().getIntegratedSensorAbsolutePosition();
     }
 
     public void setSpeedMultiplier(double speed, boolean updateNT) {
