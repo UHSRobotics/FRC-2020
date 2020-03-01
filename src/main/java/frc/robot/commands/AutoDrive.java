c7dbf5c85ab4bcaf2ef54e5c4cc36600806300ab
UHS Robotics <UHSRobotics@yrdL0MBLFYMZTP2.EDU.YRDSB.NET>
2020-03-01 13:52:57 -0500

    shuffleboard for servo

--- src/main/java/frc/robot/commands/AutoDrive.java
+++ src/main/java/frc/robot/commands/AutoDrive.java
@@ -24,13 +24,16 @@ public class AutoDrive extends CommandBase {
 
     @Override
     public void execute(){
+        m_drive.arcadeDriveAuton(-0.3, 0);
+        Timer.delay(0.5);
+        m_drive.arcadeDriveAuton(0, 0);
         m_fw.setSpeed(1);
         Timer.delay(2);
         m_hop.switchON(1);
         Timer.delay(5);
         m_fw.setSpeed(0);
         m_hop.switchOFF();
-        m_drive.arcadeDriveAuton(0.4, 0);
+        m_drive.arcadeDriveAuton(-0.3, 0);
         Timer.delay(1);
         m_drive.arcadeDriveAuton(0, 0);
         finished = true;
