e9e9c4098886ef7ad3767e7ed25fc16c7e8d30dc
Ynng <44907675+Ynng@users.noreply.github.com>
2020-03-04 00:21:26 -0500

    formatted/indentted code

--- src/main/java/frc/robot/commands/pidcommand/DistancePIDCommand.java
+++ src/main/java/frc/robot/commands/pidcommand/DistancePIDCommand.java
@@ -7,24 +7,21 @@ import frc.robot.Constants.PhysicalMeasurements;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile.*;
 
-
 public class DistancePIDCommand extends ProfiledPIDCommand {
-    //goal in cm
+    // goal in cm
     public DistancePIDCommand(DriveSubsystem drive, double goal) {
-        
-        super(new ProfiledPIDController(DriveConstants.KpDist, DriveConstants.KiDist, DriveConstants.KdDist, new Constraints(DriveConstants.velLimit,DriveConstants.accelLimit)),
-        // Close loop on heading
-        drive::getAvgEncCM,
-        // Set reference to target
-        drive.getAvgEncCM() + goal,
-        // Pipe output to turn robot
-        (output, setpoint) -> drive.arcadeDriveAuton(output,0),
-        // Require the drive
-        drive);
-        
+        super(new ProfiledPIDController(DriveConstants.KpDist, DriveConstants.KiDist, DriveConstants.KdDist,
+                new Constraints(DriveConstants.velLimit, DriveConstants.accelLimit)),
+                // Close loop on heading
+                drive::getAvgEncCM,
+                // Set reference to target
+                drive.getAvgEncCM() + goal,
+                // Pipe output to turn robot
+                (output, setpoint) -> drive.arcadeDriveAuton(output, 0),
+                // Require the drive
+                drive);
         getController().setTolerance(1);
-
-        System.out.println("goal is " + goal/(PhysicalMeasurements.wheelDiam * Math.PI) * 4096);
+        System.out.println("goal is " + goal / (PhysicalMeasurements.wheelDiam * Math.PI) * 4096);
 
         // Set the controller to be continuous (because it is an angle controller)
         // getController().enableContinuousInput(-180, 180);
