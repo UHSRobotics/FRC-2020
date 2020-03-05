cec3221a8cf026b65c0ac0ae0a02e1ada10a10cb
Cindy Zhu <cindyzhu0830@gmail.com>
2020-03-04 20:20:03 -0500

    changed port of intake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,7 @@ public final class Constants {
         public static final int flywheelInvert = 2;
         public static final int lift = 4;
         public static final int liftFollow = 9;
-        public static final int intake = 12;
+        public static final int intake = 11;
         public static final int servo = 0;
         public static final int hopper = 20;
         public static final int liftEncoderA = 6;
