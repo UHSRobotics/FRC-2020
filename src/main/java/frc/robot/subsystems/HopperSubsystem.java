8a317101361358dfbabd314ab43b469537dae939
Cindy Zhu <cindyzhu0830@gmail.com>
2020-03-07 23:36:10 -0500

    fixed import

--- src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -10,8 +10,11 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Ports;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 public class HopperSubsystem extends SubsystemBase {
 
